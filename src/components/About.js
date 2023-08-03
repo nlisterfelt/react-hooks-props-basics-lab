d8880dfd511850e968f448059eae876208e79936
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-02 19:18:10 -0500

    props and components

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+  let isString
+  if(props.bio===undefined){
+    isString = false
+  } else if(props.bio===""){
+    isString = false
+  } else {
+    isString = true
+  }
+  
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {isString ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
